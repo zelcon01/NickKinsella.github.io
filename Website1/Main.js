b8ce939136e56cfcd75537324e590778bc4ce5ca
Nicholas Kinsella <Kinsella.nick@gmail.com>
2023-05-04 18:58:10 +0200

    Add files via upload

---
+++ Website1/Main.js
@@ -0,0 +1,5 @@
+// This is a simple Javascript page
+function myFunction() {
+  var x = document.getElementById("myInput").value;
+  document.getElementById("demo").innerHTML = "You typed: " + x;
+}
